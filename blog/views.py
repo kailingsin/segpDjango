9f479dd181e46e2ecf11d0913d06ec36e86761bf
Kellieys <y10a.kelly@gmail.com>
2020-02-19 00:47:13 +0800

    Applications And Routing

---
+++ blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'Dummy data',
+        'title': 'Tomato Predicted price',
+        'content': 'Display some graph',
+        'date_posted': 'Feburary 18, 2020'
+    },
+    {
+      'author': 'Dummy data',
+        'title': 'Chicken Predicted price',
+        'content': 'Display some graph',
+        'date_posted': 'Feburary 18, 2020'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
